84a572dcd6417c57e9b7ea0b3ad37873dca73086
Shahzaib <shehkhan494@gmail.com>
2026-07-09 22:55:54 +0500

    middleware

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -41,13 +41,13 @@ const registerUser = async (req, res) => {
                 username: newUser.username,
                 email: newUser.email,
                 token: genToken(newUser._id),
-                otp: otp});
+                otp: otp
+            });
         }
 
         else {
             res.status(400).json({ message: "Invalid user data" });
         }
-
 
         await newUser.save();
         res.status(201).json({ message: "User registered successfully" });
@@ -59,9 +59,44 @@ const registerUser = async (req, res) => {
     }
 }
 
+//login user
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await Userm.findOne({ email });    
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            res.status(200).json({
+                _id: user._id,
+                username: user.username,
+                email: user.email,
+                token: genToken(user._id)
+            });
+        } else {
+            res.status(401).json({ message: "Invalid email or password" });
+        }   
+    } catch (error) {
+        console.error("Error logging in user:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+//get all user
+const getUsers = async (req, res) => {
+    try {
+        const users = await Userm.find().select('-password'); // Exclude password field from the response
+        res.status(200).json(users);
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
 
 
-export default registerUser;
+export default { registerUser, loginUser, getUsers };
+
+
 
 
 
